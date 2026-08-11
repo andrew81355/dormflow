0ec735f557de8f7d3819bf9e68cf6dbe23753280
Andrei <andreykuznetsoa10@gmail.com>
2026-08-11 10:46:45 +0500

    mongodb and error handler

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
+//connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((error) => 
+  console.error("Error connecting to MongoDB:", error));
+// get files from public folder
+app.use(express.static('public'));
 // verify that server is running(when get on /ping respond with ok)
 app.get("/ping", (request, response) => {response.json({ status: "ok" });});
+//404 handler
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Not Found" });
+});
+app.use((error, request, response, next) => {
+  console.error(error.stack);
+  response.status(500).json({ error: "Internal Server Error" });
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-// get files from public folder
-app.use(express.static('public'));
